493bf8595c1b11547a26f781e223cbd7ed6357ec
Srikanth Vinukonda <srikanthchandu.v@gmail.com>
2017-10-18 19:27:30 -0500

    converting to https

--- app/actions/index.js
+++ app/actions/index.js
@@ -4,7 +4,7 @@ import jsonp from 'jsonp';
 
 const API_KEY = '74062ad29c8de685e26ddb7362954a59';
 const API_SECRET = '055a34063699da3d226aa30a44de0c9a';
-const URL = `http://api.petfinder.com/pet.getRandom?format=json&key=${API_KEY}&animal=dog&location=76155&output=basic`;
+const URL = `https://api.petfinder.com/pet.getRandom?format=json&key=${API_KEY}&animal=dog&location=76155&output=basic`;
 
 export default function(){
   const request = fetchJsonp(URL).then(response => response.json());
